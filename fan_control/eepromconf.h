5421e78f9d8c142594413c920635b22d4241e89d
kludda <david.alind@gmail.com>
2024-05-29 21:13:25 +0200

    Update eepromconf.h

--- fan_control/eepromconf.h
+++ fan_control/eepromconf.h
@@ -4,75 +4,65 @@
 
 
 #define EEPROM_START 0
+#define EEPROM_SIZE 512
+char jsonbuf[600] = { 0 };  // buffer for json string
 int conf_initalized = 0;
 
-typedef struct _conf
-    {
-        const char *name;
-        char *data;
-        int len;
-        char *def;
-    } t_conf;
+
+typedef struct _conf {
+  const char *name;
+  char *data;
+  int len;
+  char *def;
+  int hidden;
+} t_conf;
 
 t_conf conf[] = {
-  {
-    .name = "magic_string",
+  { .name = "magic_string",
     .len = 5,
-//    .def = {0xA5,0xA5,0xA5,0xA5}
-    .def = "magi"
-  },
+    //    .def = {0xA5,0xA5,0xA5,0xA5}
+    .def = "magi" },
 
   // wifi conf
   {
     .name = "wifi_enabled",
     .len = 2,
-    .def = "0"
-  },
-  {
-    .name = "wifi_ssid",
-    .len = 33
-  },
-  {
-    .name = "wifi_password",
-    .len = 33
-  },
+    .def = "0" },
+  { .name = "wifi_ssid",
+    .len = 33 },
+  { .name = "wifi_password",
+    .len = 33,
+    .hidden = 1 },
 
   // mqtt conf
   {
     .name = "mqtt_enabled",
     .len = 2,
-    .def = "0"
-  },
-  {
-    .name = "mqtt_broker_host",
+    .def = "0" },
+  { .name = "mqtt_broker_host",
     .len = 33,
-    .def = "localhost"
-  },
-  {
-    .name = "mqtt_broker_port",
+    .def = "localhost" },
+  { .name = "mqtt_broker_port",
     .len = 5,
-    .def = "1883"
-  },
-  {
-    .name = "mqtt_root",
+    .def = "1883" },
+  { .name = "mqtt_root",
     .len = 33,
-    .def = "home/"
-  }
+    .def = "home/" }
 };
 
 void init_conf() {
   // initalize conf array
-  for (int i = 0; i < sizeof(conf)/sizeof(t_conf); i++) {
-    conf[i].data = calloc(conf[i].len, sizeof(char*));
-    if(conf[i].def != NULL) {
-      strcpy(conf[i].data,conf[i].def);
+  for (int i = 0; i < sizeof(conf) / sizeof(t_conf); i++) {
+    conf[i].data = calloc(conf[i].len, sizeof(char *));
+    if (conf[i].def != NULL) {
+      strcpy(conf[i].data, conf[i].def);
     }
   }
   // check if magic string
   //https://www.cs.umb.edu/cs341/Lab03/index.html
-  char *magic = calloc(conf[0].len, sizeof(char*));
-//  readEEPROM(EEPROM_START, conf[0].len, magic);
-  if(strcmp(magic, conf[0].data)) {
+  char *magic = calloc(conf[0].len, sizeof(char *));
+  //  readEEPROM(EEPROM_START, conf[0].len, magic);
+  if (strcmp(magic, conf[0].data)) {
     // no, write default conf to EEPROM
     write_conf();
   } else {
@@ -85,8 +75,8 @@ void init_conf() {
 
 int write_conf() {
   int mem_counter = EEPROM_START;
-  for (int i = 0; i < sizeof(conf)/sizeof(t_conf); i++) {
-    //writeEEPROM(mem_counter, conf[i].len, conf[i].data);     
+  for (int i = 0; i < sizeof(conf) / sizeof(t_conf); i++) {
+    //writeEEPROM(mem_counter, conf[i].len, conf[i].data);
     mem_counter += conf[i].len;
   }
   return 1;
@@ -94,7 +84,7 @@ int write_conf() {
 
 void read_conf() {
   int mem_counter = EEPROM_START;
-  for (int i = 0; i < sizeof(conf)/sizeof(t_conf); i++) {
+  for (int i = 0; i < sizeof(conf) / sizeof(t_conf); i++) {
     //readEEPROM(mem_counter, conf[i].len, conf[i].data);
     mem_counter += conf[i].len;
   }
@@ -102,15 +92,60 @@ void read_conf() {
 
 
 t_conf *get_conf(char *name) {
-  if(!conf_initalized) return 0;
-  for (int i = 0; i < sizeof(conf)/sizeof(t_conf); i++) {
-    if(!strcmp(name, conf[i].name)) {
+  if (!conf_initalized) return 0;
+  for (int i = 0; i < sizeof(conf) / sizeof(t_conf); i++) {
+    if (!strcmp(name, conf[i].name)) {
       return &conf[i];
     }
   }
   return NULL;
 }
 
+t_conf *get_conf_a() {
+  if (!conf_initalized) return &conf;
+  return NULL;
+}
+
+/*
+int get_conf_json(char *dest) {
+  if(!conf_initalized) return 0;
+//    char* result = calloc(1);
+
+    for (int i = 0; i < sizeof(conf)/sizeof(t_conf); i++) {
+      if(!conf[i].hidden) {
+        strlen(jsonbuf
+        char* conf = {0};
+        char* temp = {0};
+        conf = calloc(strlen(conf[i].name) + strlen(conf[i].data) + 6 * sizeof(char));
+        strcat(conf, "\"");
+        strcat(conf, conf[i].name);
+        strcat(conf, "\":\"");
+        strcat(conf, conf[i].data);
+        strcat(conf, "\",");
+        temp = calloc(strlen(dest) + strlen(conf));
+        strcpy(temp, dest);
+        strcat(temp, conf);
+        free(conf);
+        free(dest);
+        dest = calloc(strlen(temp));
+        free(temp);
+      }
+    }
+
+
+  return NULL;
+}
+*/
+
+char *astrcat(const char *x, const char *y) {
+  size_t nx = strlen(x), ny = strlen(y);
+  char *z = malloc(nx + ny + 1);
+  if (!z)
+    return NULL;
+  memcpy(z, x, nx);
+  memcpy(z + nx, y, ny + 1);
+  return z;
+}
 
 /*
 int get_conf(char *name, char *dest) {
@@ -126,10 +161,10 @@ int get_conf(char *name, char *dest) {
 */
 
 int set_conf(char *name, char *data) {
-  if(!conf_initalized) return 0;
+  if (!conf_initalized) return 0;
   int mem_counter = EEPROM_START;
-  for (int i = 0; i < sizeof(conf)/sizeof(t_conf); i++) {
-    if(!strcmp(name, conf[i].name)) {
+  for (int i = 0; i < sizeof(conf) / sizeof(t_conf); i++) {
+    if (!strcmp(name, conf[i].name)) {
       // update struct
       strcpy(conf[i].data, data);
       // write to eeprom
@@ -160,29 +195,27 @@ int main () {
 */
 
 //startAdr: offset (bytes), writeString: String to be written to EEPROM
-void writeEEPROM(int startAdr, int laenge, char* writeString) {
-  EEPROM.begin(512); //Max bytes of eeprom to use
+void writeEEPROM(int startAdr, int laenge, char *writeString) {
+  EEPROM.begin(EEPROM_SIZE);  //Max bytes of eeprom to use
   yield();
-//  Serial.println();
-//  Serial.print("writing EEPROM: ");
-  //write to eeprom 
-  for (int i = 0; i < laenge; i++)
-    {
-      EEPROM.write(startAdr + i, writeString[i]);
-//      Serial.print(writeString[i]);
-    }
+  //  Serial.println();
+  //  Serial.print("writing EEPROM: ");
+  //write to eeprom
+  for (int i = 0; i < laenge; i++) {
+    EEPROM.write(startAdr + i, writeString[i]);
+    //      Serial.print(writeString[i]);
+  }
   EEPROM.commit();
-  EEPROM.end();           
+  EEPROM.end();
 }
 
-void readEEPROM(int startAdr, int maxLength, char* dest) {
-  EEPROM.begin(512);
+void readEEPROM(int startAdr, int maxLength, char *dest) {
+  EEPROM.begin(EEPROM_SIZE);
   delay(10);
-  for (int i = 0; i < maxLength; i++)
-    {
-      dest[i] = EEPROM.read(startAdr + i);
-    }
-  EEPROM.end();    
-//  Serial.print("ready reading EEPROM:");
-//  Serial.println(dest);
+  for (int i = 0; i < maxLength; i++) {
+    dest[i] = EEPROM.read(startAdr + i);
+  }
+  EEPROM.end();
+  //  Serial.print("ready reading EEPROM:");
+  //  Serial.println(dest);
 }
